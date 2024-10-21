b837d6e35db16764fef3038fbaca4b1c648a46ae
Dor Serero <dorserero@microsoft.com>
2024-10-21 20:27:01 +0000

    add support for args

--- pkg/gadget/gadget.go
+++ pkg/gadget/gadget.go
@@ -11,9 +11,47 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	SCMP_CMP_EQ        string = "SCMP_CMP_EQ"
+	SCMP_CMP_NE        string = "SCMP_CMP_NE"
+	SCMP_CMP_LT        string = "SCMP_CMP_LT"
+	SCMP_CMP_LE        string = "SCMP_CMP_LE"
+	SCMP_CMP_GT        string = "SCMP_CMP_GT"
+	SCMP_CMP_GE        string = "SCMP_CMP_GE"
+	SCMP_CMP_MASKED_EQ string = "SCMP_CMP_MASKED_EQ"
+)
+
+func convertSeccompOperator(op seccomp.Operator) (string, error) {
+	switch op {
+	case seccomp.OpEqualTo:
+		return "==", nil
+	case seccomp.OpNotEqual:
+		return "!=", nil
+	case seccomp.OpLessThan:
+		return "<", nil
+	case seccomp.OpLessEqual:
+		return "<=", nil
+	case seccomp.OpGreaterThan:
+		return ">", nil
+	case seccomp.OpGreaterEqual:
+		return ">=", nil
+	case seccomp.OpMaskedEqual:
+		return "&", nil
+	default:
+		return "", fmt.Errorf("unsupported seccomp operator: %s", op)
+	}
+}
+
+type Arg struct {
+	Index    uint   `json:"index"`
+	Value    uint64 `json:"value"`
+	ValueTwo uint64 `json:"valueTwo"`
+	Op       string `json:"op"`
+}
+
 type Syscall struct {
 	Action   seccomp.Action
-	Args     []*seccomp.Arg
+	Args     []Arg
 	Includes seccomp.Filter
 	Excludes seccomp.Filter
 	Errno    string
@@ -73,9 +111,23 @@ func seccompToTemplateData(profileName string, seccompProfile *seccomp.Seccomp) 
 						templateProfile.Syscalls[syscallName] = []Syscall{}
 					}
 
+					args := []Arg{}
+					for _, seccompArg := range seccompSyscall.Args {
+						op, err := convertSeccompOperator(seccompArg.Op)
+						if err != nil {
+							return TemplateProfile{}, err
+						}
+
+						args = append(args, Arg{
+							Index:    seccompArg.Index,
+							Value:    seccompArg.Value,
+							ValueTwo: seccompArg.ValueTwo,
+							Op:       op,
+						})
+					}
 					syscallRule := Syscall{
 						Action:   seccompSyscall.Action,
-						Args:     seccompSyscall.Args,
+						Args:     args,
 						Includes: seccompSyscall.Includes,
 						Excludes: seccompSyscall.Excludes,
 						Errno:    seccompSyscall.Errno,
